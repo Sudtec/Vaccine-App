f609ae534f6e558444199070a9c1e4acb800629f
sudtech <sodiq@initshq.com>
2022-06-04 06:07:41 +0100

    Why Section Competed

--- src/sections/why/Why.js
+++ src/sections/why/Why.js
@@ -6,14 +6,53 @@ const Why = () => {
     return (
         <section className='container-fluid why'>
             <div className='container whyContainer'>
-            <div className="row">
-                <div className="col-md-6 whyImagesContainer">
+                <div className="row d-flex">
+                    <div className="col-md-6 whyImagesContainer">
+                        <img src={whyCall} alt='why call' className='whyCall' />
+                        <img src={docProfile} alt='doc profile' className='docProfile' />
+                    </div>
+                    <div className="col-md-6 whyContentsContainer">
+                        <div className="whyContents">
+                            <h4 className="whyHeader">
+                                Why Vaccine App?
+                            </h4>
+                            <p className="whyAnswer">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+                                aliquam, purus sit amet luctus venenatis, lectus magna fr
+                            </p>
 
+                            <div className="whyReasons">
+                                <div className="Reason">
+                                    <h5 className="whyPoint">
+                                        Easy to Use
+                                    </h5>
+                                    <p className="whyPointExplanation">
+                                        Lorem ipsum dolor sit amet, consecteturadip
+                                        iscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fr
+                                    </p>
+                                </div>
+                                <div className="Reason">
+                                    <h5 className="whyPoint">
+                                        Saves Money
+                                    </h5>
+                                    <p className="whyPointExplanation">
+                                        Lorem ipsum dolor sit amet, consecteturadip
+                                        iscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fr
+                                    </p>
+                                </div>
+                                <div className="Reason">
+                                    <h5 className="whyPoint">
+                                        24/7 Online Service
+                                    </h5>
+                                    <p className="whyPointExplanation">
+                                        Lorem ipsum dolor sit amet, consecteturadip
+                                        iscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fr
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-6 whyContainer">
-
-                </div>
-            </div>
             </div>
         </section>
     )
